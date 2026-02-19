039c38b0d467657efba3016b328a93915b4e83b8
redcr0wz <lexisario5503@gmail.com>
2026-02-19 15:38:20 -0800

    everypicture progress

---
+++ everypicture/v2-final-version/script.js
@@ -0,0 +1,9 @@
+(function () {
+    "use strict";
+    console.log("reading js");
+
+    document.querySelector('#close').addEventListener('click', function(){
+        console.log('over x');
+    });
+
+})();
