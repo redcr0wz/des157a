5f500c11009cafc0a3a80dc9f00bdb8054366e3b
redcr0wz <lexisario5503@gmail.com>
2026-02-19 16:54:54 -0800

    more everypicture dev

--- everypicture/v2-final-version/script.js
+++ everypicture/v2-final-version/script.js
@@ -6,4 +6,15 @@
         console.log('over x');
     });
 
+    document.querySelector('#pin1').addEventListener('click', function(){
+        console.log('over pin');
+    });
+
+    const header = document.querySelector('header');
+
+    setTimeout(function(){
+        header.className = 'hidden';
+    }, 7000);
+
+
 })();
